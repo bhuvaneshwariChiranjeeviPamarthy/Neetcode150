0430337ded667915b2d12605a9a24f7d832a4800
bhuvaneshwariChiranjeeviPamarthy <chiranjeevi.bhuvaneshwari.p@gmail.com>
2026-04-16 00:40:43 -0400

    Add heap solutions for KthLargest stream and Last Stone Weight

---
+++ cpp/heap-priority-queue/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,60 @@
+#include <queue>
+#include <vector>
+using namespace std;
+
+class KthLargest {
+private:
+int k;
+priority_queue<int, vector<int>, greater<int>> minHeap; //MinHeap to store the K largest elements.
+
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        // Build heap with only k largest elements
+        for(auto i: nums){
+            minHeap.push(i);
+            // If size exceeds k, remove smallest
+            if(minHeap.size() > k){
+                minHeap.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        // Add new value to heap
+        minHeap.push(val);
+        // Maintain only k elements in heap
+        while(minHeap.size() > k){
+            minHeap.pop();
+        }
+        // Top of min-heap = kth largest element
+        return minHeap.top();
+    }
+};
+
+
+// Idea:
+// Maintain a min-heap of size k.
+// Heap stores k largest elements seen so far.
+//
+// Why min-heap?
+// Smallest among these k elements = kth largest overall.
+//
+// Steps:
+// 1. Push element into heap.
+// 2. If heap size > k -> pop smallest.
+// 3. Heap always contains top k largest elements.
+// 4. Return heap.top().
+//
+// Time Complexity:
+// add() -> O(log k)
+//
+// Space:
+// O(k)
+// Worst case space can be O((n-k)logk)
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
